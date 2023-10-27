2c4da014196f00330524e53645c8c2623d40ef2c
Naufal Danadyaksa <naufal060906@gmail.com>
2023-10-27 22:22:40 +0700

    First Commit - Make Splash Loading Screen

---
+++ app/src/main/java/com/example/sportify/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.sportify;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.sportify.MainApp;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // on below line we are configuring our window to full screen
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        // on below line we are calling handler to run a task
+        // for specific time interval
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(MainActivity.this, MainApp.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 2000);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        View decorView = getWindow().getDecorView();
+
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+    }
+}
