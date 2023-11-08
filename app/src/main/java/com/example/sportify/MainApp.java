2a95b1b1c915d2b6ee9209fc9f79ce6218a6d1ba
Naufal Danadyaksa <naufal060906@gmail.com>
2023-11-08 13:14:40 +0700

    4th Commit - DONE

--- app/src/main/java/com/example/sportify/MainApp.java
+++ app/src/main/java/com/example/sportify/MainApp.java
@@ -5,8 +5,10 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
@@ -38,5 +40,31 @@ public class MainApp extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    public void lari(View view) {
+        String url="https://www.kompas.com/sports/read/2021/07/14/18000018/tips-lari-12-menit-tembus-3200-meter";
+        Uri webpage= Uri.parse(url);
+        Intent intent=new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+
+    public void renang(View view) {
+        String url="https://www.kompas.com/sports/read/2021/07/16/10300098/tips-latihan-renang-untuk-pemula";
+        Uri webpage= Uri.parse(url);
+        Intent intent=new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+    public void sepeda(View view) {
+        String url="https://lifestyle.kompas.com/read/2020/08/29/200000520/6-tips-bersepeda-aman-di-jalan-raya-kamu-sudah-tahu-?utm_source=Whatsapp&utm_medium=Referral&utm_campaign=Bottom_Mobile";
+        Uri webpage= Uri.parse(url);
+        Intent intent=new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
+    }
+    public void gym(View view) {
+        String url="https://health.kompas.com/read/23J02073100468/tak-perlu-malu-begini-tips-memulai-olahraga-di-gym-untuk-pemula";
+        Uri webpage= Uri.parse(url);
+        Intent intent=new Intent(Intent.ACTION_VIEW, webpage);
+        startActivity(intent);
     }
 }
